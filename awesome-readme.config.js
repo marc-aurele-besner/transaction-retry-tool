49bd5569ac96582c5da9642f8d8b962d5f06f210
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-10-22 17:22:23 -0400

    improve awesome-readme settings

--- awesome-readme.config.js
+++ awesome-readme.config.js
@@ -7,5 +7,61 @@ module.exports = {
     | |_| | | (_| | | | \\__ \\ (_| | (__| |_| | (_) | | | |     | | |  __/ |_| |  | |_| |      | || (_) | (_) | |
      \\__|_|  \\__,_|_| |_|___/\\__,_|\\___|\\__|_|\\___/|_| |_|     |_|  \\___|\\__|_|   \\__, |       \\__\\___/ \\___/|_|
                                                                                    __/ |                        
-                                                                                  |___/                         `
+                                                                                  |___/                         `,
+    root_license: `[![npm version](https://badge.fury.io/js/awesome-readme.svg)](https://badge.fury.io/js/awesome-readme)`,
+    root_header: `
+This Hardhat plugin add 2 tasks and 2 functions to retry transaction and get the current gas cost
+
+## Install transaction-retry-tool
+
+\`\`\`
+npm i transaction-retry-tool
+\`\`\`
+`,
+    root_body: `
+    
+
+## Tasks
+
+\`\`\`
+npx hardhat retry
+\`\`\`
+
+### Task: retry
+
+Usage: hardhat [GLOBAL OPTIONS] retry --tx-hash <STRING> --signer-key <STRING> [--gas-price <STRING>]
+
+OPTIONS:
+
+- --tx-hash             The transaction hash of the transaction to retry
+- --signer-key          The signer private key (or type hardhat to use hardhat.config private key)
+- --gas-price           Specify a gas price to retry the transaction with (default: current network gas price) (default: "")
+
+retry: Retry a transaction with the current gas price or specify one
+
+### Task: gas-cost
+
+Usage: hardhat [GLOBAL OPTIONS] gas-cost
+
+gas-cost: Get the current gas price on the selected network in wei
+
+## Functions
+
+Function allow you to use the transaction retry tool OR get the current gas cost.
+
+\`\`\`
+const { transactionRetry } = require('hardhat');
+
+transactionRetry.retry(
+    txHash: string,
+    signerKey: string,
+    gasPrice?: string
+)
+transactionRetry.gasCost()
+\`\`\`
+`,
+    root_footer: `## Don't hesitate to contribute to this project.`,
+    ignore_gitFiles: true,
+    ignore_gitIgnoreFiles: true,
+    ignore_files: ['.prettierignore']
 }
